7667ee8374961da6d2344aae56c6835d2dac4343
alonecans <80338613+alonecans@users.noreply.github.com>
2021-03-22 21:52:33 +0700

    Add files via upload

---
+++ nuy/help.js
@@ -0,0 +1,425 @@
+const help = (prefix, instagram, yt, name, pushname2, user, limitt, uptime, jam, tanggal) => { return`۝➤「 *_User info_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ Name: ${pushname2}
+۝♡ۣۜۜ፝͜͜͡͡✿➤ Limit: ${limitt}
+۝♡ۣۜۜ፝͜͜͡͡✿➤ Prefix: ${prefix}
+۝♡ۣۜۜ፝͜͜͡͡✿➤ Registeed: √
+۝♡ۣۜۜ፝͜͜͡͡✿➤ Tedaftar: ${user.length} 
+
+۝➤「 *_Mc-Bot Info_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}report
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}info
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}donasi
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}oᴡner
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}speed
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}daftar
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}limit
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}totaluser
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}bloᴄklist
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}banlist
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}premiumlist
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}bahasa
+
+۝➤「 *_Mc-Bot Doᴡnload_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tiktokstalk
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}igstalk
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}instaᴠid
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}instaimg
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}instastory 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}playmp3
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}fb 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}snaᴄk 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ytmp3
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ytmp4
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}joox
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}smule
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ssᴡeb
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}url2img
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tiktok
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}fototiktok
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}kbbi
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴡait
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}trendtᴡit
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}google
+
+۝➤「 *_Mc-Bot Creator_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}quotemaker
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}nulis
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}slide
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}trigger
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴡasted
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}draᴡing
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}gtaᴠ
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴡanted
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}laptop
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}linephoto
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄrossgun
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}raindrop
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}nightbeaᴄh
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}sketᴄh
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tts
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}gemuk
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tupai
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}bass
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}sloᴡ
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ttp
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}sticker
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}gifstike
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}toimg
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}nangis
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}peluk
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄium
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ngeᴡe
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}img2url
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tomp3
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tomp4
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}oᴄr
+
+۝➤「 *_Mc-Bot Sound_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ iri
+۝♡ۣۜۜ፝͜͜͡͡✿➤ tapi boong
+۝♡ۣۜۜ፝͜͜͡͡✿➤ bernyanyi
+۝♡ۣۜۜ፝͜͜͡͡✿➤ pale
+۝♡ۣۜۜ፝͜͜͡͡✿➤ pota
+۝♡ۣۜۜ፝͜͜͡͡✿➤ yamete
+۝♡ۣۜۜ፝͜͜͡͡✿➤ sad
+
+۝➤「 *_Mc-Bot Anime_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}neko
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}loli
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}husbu
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}neonime
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}naruto
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}minato
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}boruto
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}hinata
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}sakura
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}sasuke
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}toukaᴄhan
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}riᴢe
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}akira
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}itori
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}kurumi
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}miku
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}anime
+
+۝➤「 *_Mc-Bot Grup_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ganteng
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}cantik
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}beban
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}closetime
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}modeanime [on/off]
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}antilink [on/off]
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}antibadᴡord [on/off]
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}listbadᴡord
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴡelᴄome [on/off]
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}grup
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}oᴡnergrup
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}setpp
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}infogᴄ
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}add
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}kiᴄk 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}kiᴄktime 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}promote 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}demote
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}setname
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}setdesᴄ
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}grup
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tagme
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}hidetag
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tagall
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}mentionall
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}jadian
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}fitnah
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}listadmin
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}nsfᴡ
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}simih
+
+۝➤「 *_Mc-Bot Maker_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tahta
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}burnpape
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}8bit
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}gloᴡneon
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}gsuggest
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄandlemug
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}loᴠemss
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}mugfloᴡe
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}narutobanne
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}papeglass
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}romanᴄe
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}shadoᴡ
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}glitᴄh
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄoffe
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄandy
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}hpotte
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴡoodbloᴄk
+
+۝➤「 *_Mc-Bot Funn_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}asupan
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}anjing
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}kuᴄing
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄybepunk
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}testime
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴢodiak
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}hilih
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄeᴄan
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄogan
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}apakah
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}kapankah
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}bisakah
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}rate
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴡatak
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}hobby
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}infogempa
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}infonomor
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}quotes
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}truth
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}dare
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}katabijak
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}fakta
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}darkjokes
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}buᴄin
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}pantun
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}kataᴄinta
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}jadᴡaltᴠnoᴡ
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}randomkpop
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}hekebuᴄin
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}katailham
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}jarak
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}translate
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}pasangan 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}gantengᴄek 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄantikᴄek 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}artinama 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}pesengay 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}pbuᴄin 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}bpfont 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}textstyle 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}jadᴡaltᴠ 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}lirik 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄhord 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴡiki 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}brainly 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}resepmasakan 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}beitahoax
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}map 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}film 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}pinterest
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}infoᴄuaᴄa 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}jamdunia 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}mimpi
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}infoalamat 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}playstore
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}readmore
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}puisiimg
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}asupan
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tebakgambar
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄaklontong
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}family100
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}memeindo
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}kalkulator
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}moddroid
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}happymod
+
+۝➤「 *_Mc-Bot Sange_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄesex
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}randombokep
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}pornhub
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}xᴠideos
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}nsfᴡloli
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}nsfᴡbloᴡjob
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}nsfᴡneko
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}nsfᴡtrap
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}hentai
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}nekopoi
+
+۝➤「 *_Mc-Bot Religi* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}jadᴡalsholat
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}quran
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}quranlist
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}quransurah 
+
+۝➤「 *_Mc-Bot Other_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}beᴄrypt 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}enᴄode64
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}deᴄode64
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}enᴄode32
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}deᴄode32
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}enᴄbinary 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}deᴄbinary 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}enᴄoᴄtal 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}deᴄoᴄtal 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}hashidentifie 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}dorking 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}pastebin 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}tinyurl 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}bitly 
+
+۝➤「 *_Mc-Bot Spam_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}spamᴄall 
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}spamgmail 
+
+۝➤「 *_Mc-Bot Owner_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}addprem
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}remoᴠeprem
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}addbadᴡord
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}delbadᴡord
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}addbuᴄin
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}addaudio
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}addstiᴄke
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}setmemlimit
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}resetlimit
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}setlimit
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}setreply
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}setprefix
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}setnamebot
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}setppbot
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}bᴄ
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}bᴄgᴄ
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ban
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}unban
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}bloᴄk
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}unbloᴄk
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄlearall
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}delete
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}ᴄlone
+۝♡ۣۜۜ፝͜͜͡͡✿➤ ${prefix}leaᴠe
+
+۝➤「 *_Thanks To_* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ MANCA-BOT
+۝♡ۣۜۜ፝͜͜͡͡✿➤ FADLI-BOT
+۝♡ۣۜۜ፝͜͜͡͡✿➤ FX-SX-BOT
+
+۝➤「 *RULES MC-BOT* 」
+
+۝♡ۣۜۜ፝͜͜͡͡✿➤ GUNAKAN BOT YG BAIK
+۝♡ۣۜۜ፝͜͜͡͡✿➤ JANGAN VC/VON MC-BOT
+۝♡ۣۜۜ፝͜͜͡͡✿➤ PATUHI PERATURAN BOT/GRUP
+`
+}
+
+exports.help = help
+
+// penghitung aktif bot
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (6060));
+  var minutes = Math.floor(seconds % (6060) / 60);
+  var seconds = Math.floor(seconds % 60);
+  return `${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik`
+}
+
+// info bot 
+const bottt = (prefix) => {
+return `
+*JIKA INGIN MENGGUNAKAN MANCA BOT FOLLOW DULU IG SAYA*
+
+https://www.instagram.com/thenay_xploit_
+`
+}
+exports.bottt = bottt
+// donasi menu
+const donasi = (name) => { 
+	return `       
+╭──────「 *_ᴅᴏɴᴀꜱɪ_* 」
+┴
+│√ AXIS : 083815956151
+│√ INSTAGRAM : https://www.instagram.com/thenay_xploit_
+│
+│\`\`\`ᴘᴜʟꜱᴀ\`\`\`
+│ 5ᴋ ɪɴᴠɪᴛᴇ ʙᴏᴛ ᴋᴇ ɢʀᴜᴘ ꜱᴇʟᴀᴍᴀ 1 ʙᴜʟᴀɴ
+│FOLLOW AJA IG KU KALO GA MAU DONASI
+┬
+╰──────「 *_MC-BOT* 」
+`
+}
+exports.donasi = donasi
+// bahasa list
+const bahasa = (prefix) => {
+return `
+List Bahasa Untuk Command ${prefix}tts
+
+  af: Afrikaans
+  sq: Albanian
+  ar: Arabic
+  hy: Armenian
+  ca: Catalan
+  zh: Chinese
+  zh-cn: Chinese (Mandarin/China)
+  zh-tw: Chinese (Mandarin/Taiwan)
+  zh-yue: Chinese (Cantonese)
+  hr: Croatian
+  cs: Czech
+  da: Danish
+  nl: Dutch
+  en: English
+  en-au: English (Australia)
+  en-uk: English (United Kingdom)
+  en-us: English (United States)
+  eo: Esperanto
+  fi: Finnish
+  fr: French
+  de: German
+  el: Greek
+  ht: Haitian Creole
+  hi: Hindi
+  hu: Hungarian
+  is: Icelandic
+  id: Indonesian
+  it: Italian
+  ja: Japanese
+  ko: Korean
+  la: Latin
+  lv: Latvian
+  mk: Macedonian
+  no: Norwegian
+  pl: Polish
+  pt: Portuguese
+  pt-br: Portuguese (Brazil)
+  ro: Romanian
+  ru: Russian
+  sr: Serbian
+  sk: Slovak
+  es: Spanish
+  es-es: Spanish (Spain)
+  es-us: Spanish (United States)
+  sw: Swahili
+  sv: Swedish
+  ta: Tamil
+  th: Thai
+  tr: Turkish
+  vi: Vietnamese
+  cy: Welsh
+`
+}
+exports.bahasa = bahasa
+
+// Limit
+const limitend = (pushname2) => {
+        return`*ᴍᴀᴀғ ${pushname2} ʟɪᴍɪᴛ ʜᴀʀɪ ɪɴɪ ʜᴀʙɪꜱ*\ɴ*ʟɪᴍɪᴛ ᴅɪ ʀᴇꜱᴇᴛ ꜱᴇᴛɪᴀᴘ ᴊᴀᴍ 12:00 ᴡɪʙ ᴛᴇɴɢᴀʜ ᴍᴀʟᴀᴍ*`
+}
+
+const limitcount = (limitCounts) => {
+        return`
+Limit Kamu: *${limitCounts}*
+`
+}
+
+exports.limitend = limitend
+exports.limitcount = limitcount
